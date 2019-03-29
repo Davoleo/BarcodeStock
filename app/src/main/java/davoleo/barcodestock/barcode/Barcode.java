2f12a51250b442f303a8823ff90b6c8d360ec999
Davoleo <davoleo2001@gmail.com>
2019-03-29 16:38:49 +0100

    Base activities and classes
    I'll need to learn SQL before doing this thing o.o

---
+++ app/src/main/java/davoleo/barcodestock/barcode/Barcode.java
@@ -0,0 +1,27 @@
+package davoleo.barcodestock.barcode;
+
+/*************************************************
+ * Author: Davoleo
+ * Date / Hour: 29/03/2019 / 16:19
+ * Class: Barcode
+ * Project: BarcodeStock
+ * Copyright - © - Davoleo - 2019
+ **************************************************/
+
+public class Barcode {
+
+    private int code;
+    private String title;
+    private String description;
+    private int price;
+
+    public Barcode(int code, String title, String description, int price)
+    {
+        this.code = code;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+
+}
