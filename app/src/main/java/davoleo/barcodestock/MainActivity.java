66ffc063fd9325e92e02e4e38eddcff6506f52ee
Davoleo <davoleo2001@gmail.com>
2019-09-17 14:37:09 +0200

    :robot: Fix SearchView CastException

--- app/src/main/java/davoleo/barcodestock/MainActivity.java
+++ app/src/main/java/davoleo/barcodestock/MainActivity.java
@@ -4,12 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.view.menu.MenuItemWrapperICS;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
