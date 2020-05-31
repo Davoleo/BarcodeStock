2675deb645580c34fccc853d48bf2096a049febf
Davoleo <davoleo2001@gmail.com>
2020-05-31 18:33:17 +0200

    Scan Bar Code in ActivityAddEditBarcode

--- app/src/main/java/io/github/davoleo/barcodestock/ui/ActivityAddEditBarcode.java
+++ app/src/main/java/io/github/davoleo/barcodestock/ui/ActivityAddEditBarcode.java
@@ -2,14 +2,16 @@ package io.github.davoleo.barcodestock.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import io.github.davoleo.barcodestock.R;
 import io.github.davoleo.barcodestock.barcode.Barcode;
+import io.github.davoleo.barcodestock.scanner.BarcodeScannerActivity;
 
 public class ActivityAddEditBarcode extends AppCompatActivity {
 
@@ -18,29 +20,39 @@ public class ActivityAddEditBarcode extends AppCompatActivity {
     private Barcode selectedBarcode = null;
     private boolean editMode;
 
+    private EditText barcodeTxb;
+    private EditText titleTxb;
+    private EditText descriptionTxb;
+    private EditText priceTxb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_barcode);
 
+        barcodeTxb = findViewById(R.id.txbCode);
+        titleTxb = findViewById(R.id.txbTitle);
+        descriptionTxb = findViewById(R.id.txbDesc);
+        priceTxb = findViewById(R.id.txbPrice);
+
         editMode = getIntent().getBooleanExtra("edit", false);
 
         if (getIntent().hasExtra("barcode")) {
-            ((EditText) findViewById(R.id.txbCode)).setText(getIntent().getStringExtra("barcode"));
+            barcodeTxb.setText(getIntent().getStringExtra("barcode"));
         }
 
         if (editMode) {
             selectedBarcode = Barcode.fromBundle(getIntent().getExtras());
 
             setTitle(R.string.title_activity_edit_barcode);
-            ((Button) findViewById(R.id.button)).setText(R.string.btn_save);
+            barcodeTxb.setText(R.string.btn_save);
 
-            ((EditText) findViewById(R.id.txbCode)).setText(String.valueOf(selectedBarcode.getCode()));
-            ((EditText) findViewById(R.id.txbPrice)).setText(String.valueOf(selectedBarcode.getPrice()));
+            barcodeTxb.setText(String.valueOf(selectedBarcode.getCode()));
+            priceTxb.setText(String.valueOf(selectedBarcode.getPrice()));
 
-            ((EditText)findViewById(R.id.txbTitle)).setText(selectedBarcode.getTitle());
-            ((EditText) findViewById(R.id.txbDesc)).setText(selectedBarcode.getDescription());
+            titleTxb.setText(selectedBarcode.getTitle());
+            descriptionTxb.setText(selectedBarcode.getDescription());
 
 
         }
@@ -48,11 +60,11 @@ public class ActivityAddEditBarcode extends AppCompatActivity {
 
     public void addBarcode(View view)
     {
-        Editable txbCode = ((EditText) findViewById(R.id.txbCode)).getText();
-        Editable txbPrice = ((EditText) findViewById(R.id.txbPrice)).getText();
+        Editable txbCode = barcodeTxb.getText();
+        Editable txbPrice = priceTxb.getText();
 
-        String title = ((EditText)findViewById(R.id.txbTitle)).getText().toString();
-        String desc = ((EditText) findViewById(R.id.txbDesc)).getText().toString();
+        String title = titleTxb.getText().toString();
+        String desc = descriptionTxb.getText().toString();
 
         try {
             long code = Long.parseLong(txbCode.toString());
@@ -77,8 +89,28 @@ public class ActivityAddEditBarcode extends AppCompatActivity {
 
         }catch (NumberFormatException exception) {
             exception.printStackTrace();
-            Log.e(TAG, "addBarcode: Price or Code fields are not formatted correctly");
+            Snackbar.make(view, "Barcode/Price might not be valid! Please check them before continuing.", Snackbar.LENGTH_LONG);
+            Log.w(TAG, "addBarcode: Price or Code fields are not formatted correctly");
         }
+    }
 
+    /**
+     * Starts the scanner Activity to add a scanned barcode to the object
+     * @param view the button (view) that this method was called from
+     */
+    public void startScanner(View view) {
+        Intent intent = new Intent(getApplicationContext(), BarcodeScannerActivity.class);
+        startActivityForResult(intent, 4);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 4 && resultCode == RESULT_OK && data != null) {
+            String barcode = data.getStringExtra("barcode");
+            if (barcode != null)
+                barcodeTxb.setText(barcode);
+        }
     }
 }
