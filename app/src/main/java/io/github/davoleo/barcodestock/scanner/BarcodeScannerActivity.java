61da36934ca5a82371148f1ddf11589273834992
Davoleo <davoleo2001@gmail.com>
2020-07-10 16:09:10 +0200

    Upgrade API Level from 28 to 30 & minimum API level (19 -> 21)
    + Remove the old barcode scanning activity
    + add mlkit as a new bundled dependency

--- app/src/main/java/io/github/davoleo/barcodestock/scanner/BarcodeScannerActivity.java
+++ app/src/main/java/io/github/davoleo/barcodestock/scanner/BarcodeScannerActivity.java
@@ -1,174 +1,61 @@
 package io.github.davoleo.barcodestock.scanner;
 
 import android.Manifest;
-import android.app.ActionBar;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.MenuItem;
-import android.view.ViewGroup;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
 import io.github.davoleo.barcodestock.R;
-import me.dm7.barcodescanner.core.CameraWrapper;
-import me.dm7.barcodescanner.zbar.BarcodeFormat;
-import me.dm7.barcodescanner.zbar.Result;
-import me.dm7.barcodescanner.zbar.ZBarScannerView;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class BarcodeScannerActivity extends AppCompatActivity implements ZBarScannerView.ResultHandler{
+public class BarcodeScannerActivity extends AppCompatActivity {
 
-    private static final String FLASH_STATE = "FLASH_STATE";
-    private static final String AUTO_FOCUS_STATE = "AUTO_FOCUS_STATE";
-    private static final String SELECTED_FORMATS = "SELECTED_FORMATS";
-    private static final String CAMERA_ID = "CAMERA_ID";
+    private BarcodeScannerOptions formats = new BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(Barcode.FORMAT_EAN_13, Barcode.FORMAT_UPC_A, Barcode.FORMAT_UPC_E)
+            .build();
+    private CameraManager manager;
 
-    private Camera camera;
-    private ZBarScannerView scannerView;
-    private boolean mFlash;
-    private boolean mAutoFocus;
-    private ArrayList<Integer> mSelectedIndices;
-    private int mCameraId = -1;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode_scanner);
+
+        manager = (CameraManager) this.getSystemService(Context.CAMERA_SERVICE);
+    }
+
+
+    private boolean requestCameraPermissions() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 3);
-        }
-
-        camera = initCamera(getApplicationContext());
-
-        if (savedInstanceState != null) {
-            mFlash = savedInstanceState.getBoolean(FLASH_STATE, false);
-            mAutoFocus = savedInstanceState.getBoolean(AUTO_FOCUS_STATE, true);
-            mSelectedIndices = savedInstanceState.getIntegerArrayList(SELECTED_FORMATS);
-            mCameraId = savedInstanceState.getInt(CAMERA_ID, -1);
-        } else {
-            mFlash = false;
-            mAutoFocus = true;
-            mSelectedIndices = null;
-            mCameraId = -1;
-        }
-
-        setContentView(R.layout.activity_barcode_scanner);
-        //setupToolbar();
-        ViewGroup contentFrame = findViewById(R.id.content_frame);
-        scannerView = new ZBarScannerView(this);
-        scannerView.setupCameraPreview(CameraWrapper.getWrapper(camera, mCameraId));
-        setupFormats();
-        contentFrame.addView(scannerView);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        scannerView.setResultHandler(this);
-        scannerView.startCamera();
-        scannerView.setFlash(mFlash);
-        scannerView.setAutoFocus(mAutoFocus);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-        outState.putBoolean(FLASH_STATE, mFlash);
-        outState.putBoolean(AUTO_FOCUS_STATE, mAutoFocus);
-        outState.putIntegerArrayList(SELECTED_FORMATS, mSelectedIndices);
-        outState.putInt(CAMERA_ID, mCameraId);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        scannerView.stopCamera();
-    }
-
-    @Override
-    public void handleResult(Result rawResult) {
-
-        //Plays a notification sound
-        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
-        r.play();
-
-        //Debug Snack
-        //Snackbar.make(scannerView, "Content = " + rawResult.getContents() + " | Format = " + rawResult.getBarcodeFormat().getName(), Snackbar.LENGTH_LONG);
-
-
-        Intent intent = new Intent();
-        intent.putExtra("barcode", rawResult.getContents());
-
-        setResult(RESULT_OK, intent);
-        finish();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.home) {
-            finish();
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 
-    /**
-     * @param context The context of the application
-     * @return the Camera object or null in case the camera is unavailable
-     */
-    private Camera initCamera(Context context) {
-        Camera camera = null;
-
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
-            try {
-                camera = Camera.open();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        return camera;
-    }
-
-    public void setupFormats() {
-        List<BarcodeFormat> formats = new ArrayList<>();
-        if (mSelectedIndices == null || mSelectedIndices.isEmpty()) {
-            mSelectedIndices = new ArrayList<>();
-            mSelectedIndices.add(BarcodeFormat.UPCE.getId());
-            mSelectedIndices.add(BarcodeFormat.UPCA.getId());
-            mSelectedIndices.add(BarcodeFormat.EAN13.getId());
-        }
-
-        for (int index : mSelectedIndices) {
-            formats.add(BarcodeFormat.getFormatById(index));
-        }
-
-        if (scannerView != null) {
-            scannerView.setFormats(formats);
+    private void openCamera(int width, int height) {
+        if (requestCameraPermissions()) {
+            return;
         }
     }
 
-    public void setupToolbar() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            setSupportActionBar(toolbar);
-        }
+    private void scanBarcode() {
+        BarcodeScanner scanner = BarcodeScanning.getClient(formats);
 
-        final ActionBar actionBar = getActionBar();
-        if (actionBar != null)
-            actionBar.setDisplayHomeAsUpEnabled(true);
+        Task<List<Barcode>> result = scanner.process(null);
     }
+
 }
